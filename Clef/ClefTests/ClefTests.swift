bb7b1da26a6e653df3e1af705726000f110359ef
santi <santisozasound@gmail.com>
2026-03-29 22:38:14 -0700

    Initial commit

---
+++ Clef/ClefTests/ClefTests.swift
@@ -0,0 +1,19 @@
+//
+//  ClefTests.swift
+//  ClefTests
+//
+//  Created by Santi Soza on 3/29/26.
+//
+
+import Testing
+@testable import Clef
+
+struct ClefTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # clef
-A chord chart and lyrics transciption app for Mac
+A chord chart and lyrics transciption app for Mac.
+
+This is my first coding project so it will be rough around the edges! Open to criticism and advice of course.
